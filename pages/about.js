c954302f96ac6c59a9e8944d516885accd67f38a
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-05-31 01:41:34 +0300

    about

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,11 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, quas. Lorem ipsum dolor sit amet consectetur, 
+                adipisicing elit. Ipsa, quas. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, quas. Lorem ipsum dolor sit amet consectetur, 
+                adipisicing elit. Ipsa, quas. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, quas. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            </p>
         </div>
     )
 }
