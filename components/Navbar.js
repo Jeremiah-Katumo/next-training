b708e724709b49ae1ff32500d171ec555257ee29
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-05-31 02:03:01 +0300

    added Navbar comp

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Agent List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Agent Listing</a>
+        </nav>
+    )
+}
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa, quas. Lorem ipsum dolor sit amet consectetur, 
